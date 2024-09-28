ddc76a6a1ba37cd79f26458885b72a333e4f8ba1
Jonas Kaninda <me@jonaskaninda.com>
2024-09-28 09:54:08 +0200

    chore: update help example

--- utils/constant.go
+++ utils/constant.go
@@ -7,10 +7,10 @@
 package utils
 
 const RestoreExample = "pg-bkup restore --dbname database --file db_20231219_022941.sql.gz\n" +
-	"bkup restore --dbname database --storage s3 --path /custom-path --file db_20231219_022941.sql.gz"
+	"restore --dbname database --storage s3 --path /custom-path --file db_20231219_022941.sql.gz"
 const BackupExample = "pg-bkup backup --dbname database --disable-compression\n" +
-	"pg-bkup backup --dbname database --storage s3 --path /custom-path --disable-compression"
+	"backup --dbname database --storage s3 --path /custom-path --disable-compression"
 
 const MainExample = "pg-bkup backup --dbname database --disable-compression\n" +
-	"pg-bkup backup --dbname database --storage s3 --path /custom-path\n" +
-	"pg-bkup restore --dbname database --file db_20231219_022941.sql.gz"
+	"backup --dbname database --storage s3 --path /custom-path\n" +
+	"restore --dbname database --file db_20231219_022941.sql.gz"
